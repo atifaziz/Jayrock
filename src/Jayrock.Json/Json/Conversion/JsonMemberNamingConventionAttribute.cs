42689508b9f43ef74debc40a38dd5f28be5490d0
Chip Salzenberg <chip@pobox.com>
2011-04-17 16:27:53 -0700

    Supplement case policy with underscore policy when renaming members

--- src/Jayrock.Json/Json/Conversion/JsonMemberNamingConventionAttribute.cs
+++ src/Jayrock.Json/Json/Conversion/JsonMemberNamingConventionAttribute.cs
@@ -40,12 +40,20 @@ namespace Jayrock.Json.Conversion
         Upper,      // WORLDWIDEWEB
         Lower       // worldwideweb
     }
+    [ Serializable ]
+    public enum UnderscoreConvention
+    {
+        None,
+        Prefix,     // _WorldWideWeb   (if Pascal)
+        Separate    // World_Wide_Web  (if Pascal)
+    }
 
     [ Serializable ]
     [ AttributeUsage(AttributeTargets.Property | AttributeTargets.Field) ]
     public sealed class JsonMemberNamingConventionAttribute : Attribute, IPropertyDescriptorCustomization
     {
         private NamingConvention _convention;
+        private UnderscoreConvention _underscores;
 
         public JsonMemberNamingConventionAttribute() {}
 
@@ -54,10 +62,21 @@ namespace Jayrock.Json.Conversion
             _convention = convention;
         }
 
+        public JsonMemberNamingConventionAttribute(NamingConvention naming, UnderscoreConvention underscores)
+        {
+            _convention = naming;
+            _underscores = underscores;
+        }
+
         public NamingConvention Convention
         {
             get { return _convention; }
             set { _convention = value; }
+        }
+
+        public UnderscoreConvention Underscores {
+            get { return _underscores; }
+            set { _underscores = value; }
         }
 
         void IPropertyDescriptorCustomization.Apply(PropertyDescriptor property)
@@ -65,19 +84,41 @@ namespace Jayrock.Json.Conversion
             if (property == null) 
                 throw new ArgumentNullException("property");
 
+            if (Convention == NamingConvention.None && Underscores == UnderscoreConvention.None)
+                return;
+
             string name = property.Name;
+
+            switch (Underscores)
+            {
+                case UnderscoreConvention.Prefix:
+                    if (name.Length > 0 && name[0] != '_')
+                        name = '_' + name;
+                    break;
+                case UnderscoreConvention.Separate:
+                    for (int i = 1; i < name.Length; ++i)
+                    {
+                        if (char.IsUpper(name[i])) {
+                            name = name.Substring(0, i) + '_' + name.Substring(i);
+                            ++i;
+                        }
+                    }
+                    break;
+            }
 
             switch (Convention)
             {
                 case NamingConvention.Pascal:
-                    SetName(property, char.ToUpper(name[0], CultureInfo.InvariantCulture) + name.Substring(1)); break;
+                    name = char.ToUpper(name[0], CultureInfo.InvariantCulture) + name.Substring(1); break;
                 case NamingConvention.Camel:
-                    SetName(property, char.ToLower(name[0], CultureInfo.InvariantCulture) + name.Substring(1)); break;
+                    name = char.ToLower(name[0], CultureInfo.InvariantCulture) + name.Substring(1); break;
                 case NamingConvention.Upper:
-                    SetName(property, name.ToUpper(CultureInfo.InvariantCulture)); break;
+                    name = name.ToUpper(CultureInfo.InvariantCulture); break;
                 case NamingConvention.Lower:
-                    SetName(property, name.ToLower(CultureInfo.InvariantCulture)); break;
+                    name = name.ToLower(CultureInfo.InvariantCulture); break;
             }
+
+            SetName(property, name);
         }
 
         private static void SetName(PropertyDescriptor property, string name)
