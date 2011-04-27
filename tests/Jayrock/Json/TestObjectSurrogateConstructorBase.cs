9e374bdaae6404f3a208fdb69da7553868d7cfd8
Atif Aziz <code@raboof.com>
2011-04-27 03:08:56 +0200

    Updated file license headers

--- tests/Jayrock/Json/TestObjectSurrogateConstructorBase.cs
+++ tests/Jayrock/Json/TestObjectSurrogateConstructorBase.cs
@@ -1,12 +1,12 @@
 #region License, Terms and Conditions
 //
-// Jayrock - A JSON-RPC implementation for the Microsoft .NET Framework
+// Jayrock - JSON and JSON-RPC for Microsoft .NET Framework and Mono
 // Written by Atif Aziz (www.raboof.com)
-// Copyright (c) Atif Aziz. All rights reserved.
+// Copyright (c) 2005 Atif Aziz. All rights reserved.
 //
 // This library is free software; you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation; either version 2.1 of the License, or (at your option)
+// Software Foundation; either version 3 of the License, or (at your option)
 // any later version.
 //
 // This library is distributed in the hope that it will be useful, but WITHOUT
